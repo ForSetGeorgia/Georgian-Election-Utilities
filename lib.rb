e025ede5df87e95bba09614d972c16f57385d7db
Eric Barrett <eric@barrett.works>
2017-10-18 15:05:24 +0400

    backing up scripts to date

--- lib.rb
+++ lib.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 
 def arg_check(path_to_shapefiles)
   unless path_to_shapefiles
-    STDERR.puts("Error: Missing path to shapefiles argument.")
+    STDERR.puts("Error: Missing path to shape file argument.")
     exit(false)
   end
 end
@@ -27,6 +27,16 @@ def check_dependencies(dependency_array)
    end
 end
 
+def base_path(path_to_shapefiles)
+  sep = path_to_shapefiles.split("/"); sep.pop
+  path = sep.join("/")
+end
+
+def base_name(path_to_shapefiles)
+  bname = path_to_shapefiles.split("/")[-1].sub('.shp', '')
+end
+
+# standardize filenames to lowercase
 def rename_shapefiles(path_to_shapefiles)
   files = Dir.glob(path_to_shapefiles + '*').select { |f| File.file?(f) }
   files.each { |f| FileUtils.mv(f, f.gsub(' ', '').downcase)  unless f == f.gsub(' ', '').downcase }
@@ -37,21 +47,41 @@ end
 def copy_nonsimplified_files(path_to_shapefiles)
   extensions = ['.cpg', '.sbn', '.sbx', '.shp.xml']
   extensions.each do |e|
-    file = Dir.glob(path_to_shapefiles + '*' + e)[0]
+    path = base_path(path_to_shapefiles)
+    file = Dir.glob(path + "/" + '*' + e)[0]
     unless file.nil? || file.empty?
-      FileUtils.cp(file, path_to_shapefiles + File.basename(file, e) + '_simplified' + e)
+      FileUtils.cp(file, path + "/" + File.basename(file, e) + '_simplified' + e)
     end
   end
 end
 
 def simplify_shapefiles(path_to_shapefiles, percent)
   shapefile = Dir.glob(path_to_shapefiles + '*.shp')[0]
-  `mapshaper #{shapefile} auto-snap -simplify #{percent}% -o force #{path_to_shapefiles + File.basename(shapefile, '.shp') + "_simplified.shp"}`
+  path = base_path(path_to_shapefiles)
+  `mapshaper #{shapefile} auto-snap -simplify #{percent}% -o force #{path + "/" + File.basename(shapefile, '.shp') + "_simplified.shp"}`
   copy_nonsimplified_files(path_to_shapefiles)
 end
 
 def shape2json(path_to_shapefiles)
-  shapefile = Dir.glob(path_to_shapefiles + '*_simplified.shp')[0]
-  bname = File.basename(shapefile, '.shp')
-  `ogr2ogr -f GeoJSON -t_srs crs:84 #{path_to_shapefiles + bname}.geojson #{shapefile}`
+  path = base_path(path_to_shapefiles)
+  bname = base_name(path_to_shapefiles)
+  `ogr2ogr -f GeoJSON -t_srs crs:84 #{path + "/" + bname}.geojson #{path + "/" + bname + ".shp"}`
+end
+
+def string_eval(string, properties)
+  if string.include?("[")
+    eval(string)
+  else
+    string
+  end
+end
+
+def district_id2name(id, features, id_label, name_label)
+  features.each do |feature|
+    properties = feature["properties"]
+    if id = properties[id_label]
+      return properties[name_label]
+      break
+    end
+  end
 end
